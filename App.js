d132e6811cf7d4dda1884665ade84162e9708f92
raghuvaranlokti <raghuvaranlokati@gmail.com>
2023-08-16 08:24:02 +0530

    added extra

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import Login from './Login';
 import Home from './Home';
+import LoadingScreen from './LoadingScreen'; // Import your loading screen component
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // Add loading state
 
   const handleLogin = () => {
-    setIsLoggedIn(true);
+    setIsLoading(true); // Show loading screen
+    // Simulate login process
+    setTimeout(() => {
+      setIsLoggedIn(true);
+      setIsLoading(false); // Hide loading screen
+    }, 2000); // Simulated delay of 2 seconds
+    
+    
   };
 
   const handleLogout = () => {
@@ -16,9 +25,17 @@ const App = () => {
 
   return (
     <View style={{ flex: 1 }}>
-      {isLoggedIn ? <Home onLogout={handleLogout} /> : <Login onLogin={handleLogin} />}
+      {/* Render loading screen while isLoading is true */}
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        // Render Home or Login components based on isLoggedIn state
+        isLoggedIn ? <Home onLogout={handleLogout} /> : <Login onLogin={handleLogin} />
+      )}
     </View>
   );
 };
 
 export default App;
+
+ 
--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const webLinks = [
@@ -8,57 +8,68 @@ const webLinks = [
   { title: 'get phone details', url: 'https://iplogger.org/' },
   { title: 'hacker keyboad', url: 'https://hackertyper.net/' },
   { title: 'tor (dark web )', url: 'https://support.torproject.org/tbb/' },
-  { title: 'whatsapp prank (soon)', url: ''  },
-
+  { title: 'whatsapp prank (soon)', url: '' },
   // Add more links as needed
 ];
 
 export default function Home({ onLogout }) {
   const [selectedLinkIndex, setSelectedLinkIndex] = React.useState(null);
+  const [showBackButton, setShowBackButton] = React.useState(false); // State to show/hide back button
 
   const renderWebView = (index) => {
     setSelectedLinkIndex(index);
+    setShowBackButton(true);
   };
 
   const renderLinkList = () => {
     setSelectedLinkIndex(null);
+    setShowBackButton(false);
   };
 
   const handleLogout = () => {
-    // Implement your logout logic here
-    // For example, you can clear any user session, reset state, etc.
-    onLogout(); // Call the onLogout function provided as a prop
+    onLogout();
   };
 
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.container}>
-        {selectedLinkIndex === null ? (
-          webLinks.map((link, index) => (
-            <TouchableOpacity
-              key={index}
-              onPress={() => renderWebView(index)}
-              style={styles.linkButton}
-            >
-              <Text>{link.title}</Text>
-            </TouchableOpacity>
-          ))
-        ) : (
-          <>
+      {selectedLinkIndex !== null ? null : (
+        <View style={styles.navBar}>
+          <Text style={styles.navTitle}>fogo</Text>
+          <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
+            <Text style={styles.logoutButtonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {selectedLinkIndex === null ? (
+        webLinks.map((link, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => renderWebView(index)}
+            style={styles.linkButton}
+          >
+            <Text>{link.title}</Text>
+          </TouchableOpacity>
+        ))
+      ) : (
+        <>
+          {showBackButton && (
             <TouchableOpacity onPress={renderLinkList} style={styles.backButton}>
               <Text style={styles.backButtonText}>Back</Text>
             </TouchableOpacity>
-            <WebView
-              source={{ uri: webLinks[selectedLinkIndex].url }}
-              style={styles.webView}
-              onError={(error) => console.error('WebView Error:', error)}
-            />
-          </>
-        )}
-      </ScrollView>
-      {selectedLinkIndex === null && (
-        <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
-          <Text style={styles.logoutButtonText}>Logout</Text>
+          )}
+          <WebView
+            source={{ uri: webLinks[selectedLinkIndex].url }}
+            style={styles.webView}
+            onError={(error) => console.error('WebView Error:', error)}
+          />
+        </>
+      )}
+      {selectedLinkIndex !== null ? null : (
+        <TouchableOpacity
+          onPress={() => Linking.openURL('https://www.youtube.com/@white.hacker8')}
+          style={styles.footerLink}
+        >
+          <Text style={styles.footerLinkText}>Visit our YouTube Channel</Text>
         </TouchableOpacity>
       )}
     </View>
@@ -68,6 +79,7 @@ export default function Home({ onLogout }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-top',
   },
   linkButton: {
     backgroundColor: '#EEEEEE',
@@ -75,10 +87,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 5,
     alignItems: 'center',
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
   },
   webView: {
     flex: 1,
-    height: 3000, // Adjust the height as needed
+    height: 3000,
   },
   backButton: {
     position: 'absolute',
@@ -87,21 +102,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#ADD8E6',
     padding: 10,
     borderRadius: 5,
-    zIndex: 1,
+    zIndex: 2,
   },
   backButtonText: {
     color: 'white',
     fontSize: 16,
   },
   logoutButton: {
-    backgroundColor: '#FF0000',
-    padding: 15,
-    borderRadius: 5,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 6,
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 10,
+    marginTop: 10,
   },
   logoutButtonText: {
+    color: 'lightblue',
+    fontSize: 16,
+  },
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    paddingHorizontal: 16,
+    paddingTop: 40,
+  },
+  navTitle: {
     color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  footerLink: {
+    alignItems: 'center',
+    marginTop: 200,
+  },
+  footerLinkText: {
+    color: 'blue',
     fontSize: 16,
   },
 });
